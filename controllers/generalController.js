755c687c43db8898be559adaa3897ddb349a58ac
Clara Battesini <clarabattesini@gmail.com>
2023-03-11 19:16:12 -0500

    feat: post signup method

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -22,6 +22,55 @@ router.get("/signup", (req, res) => {
     res.render("general/signup");
 });
 
+router.post("/signup", (req, res) => {
+    const { firstname, lastname, email, password } = req.body;
+
+    var responseObj = {
+        validationMsg: {},
+        values: req.body
+    };
+
+    isValidationOk = true;
+
+    if (typeof password !== "string" || password.trim().length === 0){
+        responseObj.validationMsg.password = "Please enter a password";
+        isValidationOk = false;
+    } 
+    if (!(password.trim().length >= 8 && password.trim().length <= 12 && /[a-z]+[A-Z]+[-!$@#%^&*()_+|~=`{}\[\]:";'<>?,.\/]+[0-9]+/gm.test(password))) {
+        responseObj.validationMsg.password = "A password must attend those criteria: Size is between 8 to 12 characters and contains at least one lowercase letter, uppercase letter, number and a symbol.";
+        isValidationOk = false;
+    }
+
+    
+    if (typeof email !== "string" || email.trim().length === 0){
+        responseObj.validationMsg.email = "Please enter an email";
+        isValidationOk = false;
+    } 
+
+    if (!/(\w+)@([a-z]+)\.[a-z]{2,4}/.test(email)) {
+        responseObj.validationMsg.email = "The email is not valid";
+        isValidationOk = false;
+    }
+
+    if (typeof firstname !== "string" || firstname.trim().length === 0){
+        responseObj.validationMsg.firstname = "Please enter a firstname";
+        isValidationOk = false;
+    }
+
+    if (typeof lastname !== "string" || lastname.trim().length === 0){
+        responseObj.validationMsg.lastname = "Please enter a lastname";
+        isValidationOk = false;
+    }
+
+    if (isValidationOk) {
+        
+        // SEND EMAIL
+
+    } else {
+        res.render("general/signup", responseObj);
+    }
+});
+
 router.post("/login", (req, res) => {
 
     const { email, password } = req.body;
