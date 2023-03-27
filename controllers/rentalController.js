b5929e56e27fac66c08d953c8169e6c8f42f64a6
Clara Battesini <clarabattesini@gmail.com>
2023-03-27 19:43:46 -0400

    chore: reorganizing views

--- controllers/rentalController.js
+++ controllers/rentalController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const rentals = require("../models/rental-db")
 
 router.get("/", (req, res) => {
-    res.render("general/rentals", {
+    res.render("rentals/rentals", {
         rentals: rentals.getRentalsByCityAndProvince(),
     });
 });
