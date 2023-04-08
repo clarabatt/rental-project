daa0396b0bee924321b40ea7663c5f761a3de322
Clara Battesini <clarabattesini@gmail.com>
2023-04-08 13:40:31 -0400

    feat: created the /add route

--- controllers/rental.js
+++ controllers/rental.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const rentals = require("../models/rental-db")
+// const rentals = require("../models/rental-db");
+const rentals = require("../models/rentalModel");
 
 router.get("/", (req, res) => {
     res.render("rentals/rentals", {
@@ -8,14 +9,56 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/list", (req, res) => {
-    if (!req.session.user || !req.session.isCustomer) {
-        res.render("rentals/list");
+router.get("/list", async (req, res) => {
+    if (req.session && req.session.user && !req.session.isCustomer) {
+        const data = await rentals.find().sort({headline: "asc"}).lean();
+        res.render("rentals/list", {
+            rentals: data
+        });
     } else {
-        // res.status(401).send("You are not authorized to view this page.");
         res.status(401).render("general/401");
     }
 });
+
+router.get('/add', (req, res) => {
+    res.render('rentals/add-rental');
+});
+  
+router.post('/add', async (req, res) => {
+
+    const {headline, numSleeps, numBedrooms, numBathrooms, pricePerNight, city, province, filename, site, picUrl, author, featuredRental} = req.body;
+
+    try {
+        const resp = await rentals.create({
+        headline: headline,
+        numSleeps: numSleeps,
+        numBedrooms: numBedrooms,
+        numBathrooms: numBathrooms,
+        pricePerNight: pricePerNight,
+        city: city,
+        province: province,
+        imageUrl: '/img/houses/' + filename,
+        featuredRental: featuredRental === 'on' ? true : false
+        });
+        if (resp) {
+            res.redirect('/rentals/list');
+        }
+    }  catch (err) {
+        console.log(err);
+        res.status(500).send('Error adding rental to database!');
+    }
+});
+
+router.get('/edit/:id', (req, res) => {
+    rentals.findById(req.params.id, (err, rental) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error retrieving rental from database!');
+      } else {
+        res.render('rentals/edit', { rental: rental });
+      }
+    }).lean();
+  });
 
 
 module.exports = router;
